fadf57b385081bc2c370c57fa424d702448b40bc
Erik-Martus <ekm66@drexel.edu>
2021-01-12 21:36:09 -0500

    :heavy_plus_sign: Add sass

--- webpack.common.js
+++ webpack.common.js
@@ -33,8 +33,15 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
       },
       {
         test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
