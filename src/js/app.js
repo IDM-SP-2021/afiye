f4997e61bd695b7f81a85027ff7b2f9cf57d3704
Erik-Martus <ekm66@drexel.edu>
2021-04-21 19:17:49 -0400

    :bug: Fix asset downloads not working

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,7 @@ const requireAll = (r) => {
   r.keys().forEach(r);
 };
 requireAll(require.context('../views/', true, /\.ejs$/));
-requireAll(require.context('../assets/', true, /\.(png|jpe?g|gif|svg)$/i));
+requireAll(require.context('../assets/', true, /\.(png|jpe?g|gif|svg|eps|pdf)$/i));
 requireAll(require.context('../fonts/', true, /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/));
 
 $(() => {
